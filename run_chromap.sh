93796d30671b2532cf247e79c1a2137f3b006382
keen-laras <kinanti.seraphina@gmail.com>
2025-12-15 23:58:07 +0800

    Update run_chromap.sh

--- run_chromap.sh
+++ run_chromap.sh
@@ -7,28 +7,22 @@ HIC_DIR="/path/to/HicFiles"
 THREADS=32
 
 # 工具路径
-SAMTOOLS="/share/app/samtools/1.11/bin/samtools"
+SAMTOOLS="/samtools"
 
 # 输出前缀
 OUT_PREFIX="{output}"
 
 echo "Indexing contig assembly"
 $SAMTOOLS faidx $CONTIGS
-/opt/software/chromap_env/bin/chromap -i -r $CONTIGS -o ${OUT_PREFIX}.index
+/chromap -i -r $CONTIGS -o ${OUT_PREFIX}.index
 
-# 三对 Hi-C 数据编号
-#SAMPLES=("687" "688" "689")
-
-# 循环处理三对
-#for ID in "${SAMPLES[@]}"; do
-#    echo "=== Step 2: 处理Hi-C数据对 ${ID} ==="
     R1="${HIC_DIR}/{HiC}_R1.fastq"
     R2="${HIC_DIR}/{HiC}_R2.fastq"
     
     OUT="${OUT_PREFIX}}"
 
     echo "  -> Reading ${R1} and ${R2}"
-    /opt/software/chromap_env/bin/chromap \
+    chromap \
       --preset hic \
       -r $CONTIGS \
       -x ${OUT_PREFIX}.index \
@@ -49,8 +43,8 @@ done
 
 echo "Convert bed file from Bam file for downstream yahs analysis"
 
-/share/app/samtools/1.11/bin/samtools view -bh -F 0xF0C -q 0 ${OUT}.bam | \
-/share/app/bedtools/2.29.2/bin/bedtools bamtobed | \
+/samtools view -bh -F 0xF0C -q 0 ${OUT}.bam | \
+/bedtools bamtobed | \
   awk -v OFS='\t' '{$4=substr($4,1,length($4)-2); print}' \
   > {output}_chromap.bed
 
